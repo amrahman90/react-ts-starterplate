e8836b8c5d419a75a8d6740bef4c3501d2b9f9f9
amrahman009 <mahmud.aero@gmail.com>
2020-12-29 17:25:20 +0100

    clean up errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,26 @@
 import React from 'react';
-import type { FC } from 'react';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import { create } from 'jss';
-import rtl from 'jss-rtl';
-import MomentUtils from '@date-io/moment';
-import { SnackbarProvider } from 'notistack';
-import {
-  jssPreset,
-  StylesProvider,
-  ThemeProvider
-} from '@material-ui/core';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import GlobalStyles from 'src/components/GlobalStyles';
-import ScrollReset from 'src/components/ScrollReset';
-import CookiesNotification from 'src/components/CookiesNotification';
-import GoogleAnalytics from 'src/components/GoogleAnalytics';
-import SettingsNotification from 'src/components/SettingsNotification';
-import { AuthProvider } from 'src/contexts/JWTAuthContext';
-import useSettings from 'src/hooks/useSettings';
-import { createTheme } from 'src/theme';
-import routes, { renderRoutes } from 'src/routes';
+import logo from './logo.svg';
+import './App.css';
 
-const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
-const history = createBrowserHistory();
-
-const App: FC = () => {
-  const { settings } = useSettings();
-
-  const theme = createTheme({
-    direction: settings.direction,
-    responsiveFontSizes: settings.responsiveFontSizes,
-    theme: settings.theme
-  });
-
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <StylesProvider jss={jss}>
-        <MuiPickersUtilsProvider utils={MomentUtils}>
-          <SnackbarProvider
-            dense
-            maxSnack={3}
-          >
-            <Router history={history}>
-              <AuthProvider>
-                <GlobalStyles />
-                <ScrollReset />
-                <GoogleAnalytics />
-                {/* <CookiesNotification /> */}
-                {/* <SettingsNotification /> */}
-                {renderRoutes(routes)}
-              </AuthProvider>
-            </Router>
-          </SnackbarProvider>
-        </MuiPickersUtilsProvider>
-      </StylesProvider>
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,17 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-import 'react-perfect-scrollbar/dist/css/styles.css';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import 'react-quill/dist/quill.snow.css';
-import 'prismjs/prism';
-import 'prismjs/components/prism-bash';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/components/prism-jsx';
-import 'nprogress/nprogress.css';
-import 'src/__mocks__';
-import 'src/assets/css/prism.css';
-import 'src/mixins/chartjs';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { enableES5 } from 'immer';
-import * as serviceWorker from './serviceWorker';
-import store from './store';
-import { SettingsProvider } from './contexts/SettingsContext';
+import './index.css';
 import App from './App';
-
-enableES5();
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <SettingsProvider>
-      <App />
-    </SettingsProvider>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-serviceWorker.register();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
